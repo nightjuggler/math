29ff0ab45421f7f384ecb6d1e0f3e50b5432ceb2
Pius Fischer <pius@nightjuggler.com>
2020-01-08 20:18:51 -0800

    Allow empty list parameters to be specified on the command-line, e.g. "" or ","

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 def ordinal(n):
 	return str(n) + ("th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th")[n % 10]
 
+def param_must_be(i, description):
+	print "The {} parameter after the command must be {}!".format(ordinal(i + 1), description)
+
 def parse_number(arg, convert=int):
 	try:
 		return convert(arg)
 	except ValueError:
 		return None
+
+def parse_list(arg):
+	param = []
+
+	arg = arg.strip(",")
+	if arg == "":
+		return param
+
+	for arg in arg.split(","):
+		arg = parse_number(arg)
+		if arg is None:
+			return None
+		param.append(arg)
+
+	return param
 
 def run_command(args, default_command, commands):
 	command = args.pop(0) if args else default_command
@@ -28,27 +46,23 @@ def run_command(args, default_command, commands):
 			elif arg in ("0", "off", "false", "no"):
 				param = False
 			else:
-				print "The", ordinal(i+1), "parameter after the command must be a boolean!"
+				param_must_be(i, "a boolean")
 				return
 		elif isinstance(param, int):
 			param = parse_number(arg)
 			if param is None:
-				print "The", ordinal(i+1), "parameter after the command must be an integer!"
+				param_must_be(i, "an integer")
 				return
 		elif isinstance(param, float):
 			param = parse_number(arg, float)
 			if param is None:
-				print "The", ordinal(i+1), "parameter after the command must be a number!"
+				param_must_be(i, "a number")
 				return
 		elif isinstance(param, (list, tuple)):
-			param = []
-			for arg in arg.split(","):
-				arg = parse_number(arg)
-				if arg is None:
-					print "The", ordinal(i+1), ("parameter after the command must be "
-						"a comma-separated list of integers!")
-					return
-				param.append(arg)
+			param = parse_list(arg)
+			if param is None:
+				param_must_be(i, "a comma-separated list of integers")
+				return
 		else:
 			print "Unable to parse the", ordinal(i+1), "parameter after the command!"
 			return
